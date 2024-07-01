3dfbf1b6614d2ec98a7aa478975fb7865df9d580
aldrake12 <aldrake12@gmail.com>
2024-07-01 22:55:07 +0000

    Upload of HW4-16

--- hw4-16.cpp
+++ hw4-16.cpp
@@ -1,5 +1,5 @@
 //Advanced16.cpp
-//Created/revised by <your name> on <current date>
+//Revised by Andrew Drake on 06/30/2024
 
 #include <iostream>
 #include <iomanip>
@@ -7,9 +7,32 @@ using namespace std;
 
 int main()
 {
+    //declare constant and variables
+    const double increase = 1.05;
+    double store1 = 0.0;
+    double store2 = 0.0;
+    //double total  = 0.0;
+    int store1Int = 0;
+    int store2Int = 0;
+    int totalInt = 0;
 
-  return 0;
-}	//end of main function
+    cout << "Store 1 sales: ";
+    cin >> store1;
+    cout << "Store 2 sales: ";
+    cin >> store2;
+
+    store1Int = static_cast<int>(store1 * increase * 100 + .5);
+    store2Int = static_cast<int>(store2 * increase * 100 + .5);
+    totalInt = store1Int + store2Int;
+
+    cout << fixed << setprecision(2) << endl;
+    cout << store1Int / 100.0 << " --->Store 1" << endl;
+    cout << store2Int / 100.0 << " --->Store 2" << endl;
+    cout << "-----------------" << endl;
+    cout << totalInt / 100.0 << " --->Total" << endl;
+
+    return 0;
+}    //end of main function
 
 //1.  Declare and initalize three int variables named store1Int, store2Int, and totalInt
 //2.  Change the first assignment statement to store1Int = static_cast<int>(store1 * increase * 100 + .5);
